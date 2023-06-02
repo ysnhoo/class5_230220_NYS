26e0c24db0a0049e351d551dd0d4bba834368f9d
user <user@ITWILL>
2023-06-02 10:50:52 +0900

    2번째 테스트 수정

--- GIT_Test_NYS/src/main/java/com/itwill/git/HomeController.java
+++ GIT_Test_NYS/src/main/java/com/itwill/git/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		//ㅁ너ㅏ어ㅣㅏㅁ너이ㅏㅓㅁ니ㅏ어ㅣㅁ너ㅣㅏㅇ
 		return "home";
 	}
 	
